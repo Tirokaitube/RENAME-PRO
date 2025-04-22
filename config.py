6bd56671344936b2313337ae0ddf57d38ef93870
Tirokaitube <tirokaitube@gmail.com>
2025-04-22 07:36:56 +0530

    Update config.py

--- config.py
+++ config.py
@@ -13,7 +13,7 @@ LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "-1002568229104"))
 DB_URL = os.environ.get("DB_URL", "mongodb+srv://tirokaitube:ymDe3VLWXQ05JazI@cluster0.tktl52k.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 DB_NAME = os.environ.get("DB_NAME", "tirokaitube")
 
-STRING = os.environ.get("STRING", "")
+STRING = os.environ.get("STRING", "BQGlhLEARnzKQUO9fhjjo65GWUVbf2OuowzqJYhqZDUclW029UEa4ix4Zl8ejr0pL8jdPkdCxPN8fgWzBxNSBniIGxfiqdvziRNweZ_PlI__1jsEVnD65pgLzdz8-ugPg8W5Fy5AGabAEzZ63AsLCIZk7DerIgcFx_NwEkjJ7y19eRYWybaHdFhVvPhjvVniCIRdVgI6Fc-dc9dztE2ZWRYWKjkb0wgPVRdgJwSRy5AtFmbHyeLfwsvRUkGUmES9jjVxzPZI2lxXu6D_-Rik0ryL0J02LvJoixGwYfXTcV3v3oh6S3jCEEdJtPDHKUsrLWDuRGZ7xcwrqNrIVt7ulkqauJE6FQAAAAGdpDQAAA")
 BOT_PIC = os.environ.get("BOT_PIC", "https://graph.org/file/cca849a2f63053fa3f622.jpg")
 
 
