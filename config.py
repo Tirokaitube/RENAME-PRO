ccd17032b7e36bf5cfb38fab3c71b2db5f6336b3
Tirokaitube <tirokaitube@gmail.com>
2025-04-22 07:12:38 +0530

    Update config.py

--- config.py
+++ config.py
@@ -4,14 +4,14 @@ import os, re
 BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
 API_ID = int(os.environ.get("API_ID", ""))
 API_HASH = os.environ.get("API_HASH", "")
-OWNER = int(os.environ.get("OWNER", ""))
-BOT_USERNAME = os.environ.get('BOT_USERNAME', "")
+OWNER = int(os.environ.get("OWNER", "5953621837"))
+BOT_USERNAME = os.environ.get('BOT_USERNAME', "@V2_File_Renamer_bot")
 
-FORCE_SUBS = os.environ.get("FORCE_SUBS", "HxBots")
-LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "-1002031041556"))
+FORCE_SUBS = os.environ.get("FORCE_SUBS", "Anime_Lumino")
+LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "-1002568229104"))
 
-DB_URL = os.environ.get("DB_URL", "")
-DB_NAME = os.environ.get("DB_NAME", "renamebot-premium")
+DB_URL = os.environ.get("DB_URL", "mongodb+srv://tirokaitube:ymDe3VLWXQ05JazI@cluster0.tktl52k.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+DB_NAME = os.environ.get("DB_NAME", "tirokaitube")
 
 STRING = os.environ.get("STRING", "")
 BOT_PIC = os.environ.get("BOT_PIC", "https://graph.org/file/cca849a2f63053fa3f622.jpg")
